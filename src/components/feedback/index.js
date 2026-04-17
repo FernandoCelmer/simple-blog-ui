b82f5dc4c34b8f509df83b9c648a6c0cdd8732ef
Fernando Celmer <email@fernandocelmer.com>
2026-04-17 18:44:26 -0300

    ⚙️ FEATURE: Export feedback barrel

---
+++ src/components/feedback/index.js
@@ -0,0 +1,4 @@
+export { SbAlert } from './sb-alert/sb-alert.js';
+export { SbSpinner } from './sb-spinner/sb-spinner.js';
+export { SbProgress } from './sb-progress/sb-progress.js';
+export { SbToast } from './sb-toast/sb-toast.js';
