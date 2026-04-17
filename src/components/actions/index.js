a38be5375519277bbbe513306ab30824333d285d
Fernando Celmer <email@fernandocelmer.com>
2026-04-17 18:44:05 -0300

    ⚙️ FEATURE: Export actions barrel

---
+++ src/components/actions/index.js
@@ -0,0 +1,4 @@
+export { SbButton } from './sb-button/sb-button.js';
+export { SbIconButton } from './sb-icon-button/sb-icon-button.js';
+export { SbButtonGroup } from './sb-button-group/sb-button-group.js';
+export { SbCloseButton } from './sb-close-button/sb-close-button.js';
