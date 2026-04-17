5fe393a702c3c7cef79dace1159e9997ab8a4f8a
Fernando Celmer <email@fernandocelmer.com>
2026-04-17 18:44:32 -0300

    ⚙️ FEATURE: Export navigation barrel

---
+++ src/components/navigation/index.js
@@ -0,0 +1,6 @@
+export { SbLink } from './sb-link/sb-link.js';
+export { SbTabs } from './sb-tabs/sb-tabs.js';
+export { SbNav } from './sb-nav/sb-nav.js';
+export { SbNavbar } from './sb-navbar/sb-navbar.js';
+export { SbBreadcrumb } from './sb-breadcrumb/sb-breadcrumb.js';
+export { SbPagination } from './sb-pagination/sb-pagination.js';
