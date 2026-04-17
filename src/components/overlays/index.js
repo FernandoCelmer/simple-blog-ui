a990f524d942eb96876b2df1cb755c91a330723a
Fernando Celmer <email@fernandocelmer.com>
2026-04-17 18:44:33 -0300

    ⚙️ FEATURE: Export overlays barrel

---
+++ src/components/overlays/index.js
@@ -0,0 +1,5 @@
+export { SbModal } from './sb-modal/sb-modal.js';
+export { SbTooltip } from './sb-tooltip/sb-tooltip.js';
+export { SbDropdown } from './sb-dropdown/sb-dropdown.js';
+export { SbOffcanvas } from './sb-offcanvas/sb-offcanvas.js';
+export { SbPopover } from './sb-popover/sb-popover.js';
